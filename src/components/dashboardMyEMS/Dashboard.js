60ff26ca31b5b245d70c6502a16ea01c87da5edd
guangyuzhang <13621160019@163.com>
2020-07-13 16:18:48 +0800

    updated dashboard

--- src/components/dashboardMyEMS/Dashboard.js
+++ src/components/dashboardMyEMS/Dashboard.js
@@ -1,16 +1,16 @@
 import React, { Fragment, useEffect, useState } from 'react';
-import { Row, Col, Card, CardBody, Button, InputGroup, CustomInput } from 'reactstrap';
+import { Row, Col, Card, CardBody, CardHeader, Button, InputGroup, CustomInput } from 'reactstrap';
 import CountUp from 'react-countup';
 import CardSummary from '../dashboard/CardSummary';
 import ActiveUsersBarChart from '../dashboard/ActiveUsersBarChart';
-import PaymentsLineChart from '../dashboard/PaymentsLineChart';
+import EnergysLineChart from './EnergysLineChart';
 import { toast } from 'react-toastify';
 import FalconCardHeader from '../common/FalconCardHeader';
 import ButtonIcon from '../common/ButtonIcon';
 
 import loadable from '@loadable/component';
 import DashBoardDepositStatus from '../dashboard/DashboardDepositStatus';
-const PurchasesTable = loadable(() => import('../dashboard/PurchasesTable'));
+const ChildSpacesTable = loadable(() => import('./ChildSpacesTable'));
 const ActiveUsersMap = loadable(() => import('../dashboard/ActiveUsersMap'));
 
 const Dashboard = () => {
@@ -53,48 +53,9 @@ const Dashboard = () => {
           <CountUp end={43594} duration={2} prefix="" separator="," decimal="." />
         </CardSummary>
       </div>
-      <PaymentsLineChart />
-      <DashBoardDepositStatus />
-      <Card className="mb-3">
-        <FalconCardHeader title="Recent Purchases" light={false}>
-          {isSelected ? (
-            <InputGroup size="sm" className="input-group input-group-sm">
-              <CustomInput type="select" id="bulk-select">
-                <option>Bulk actions</option>
-                <option value="Refund">Refund</option>
-                <option value="Delete">Delete</option>
-                <option value="Archive">Archive</option>
-              </CustomInput>
-              <Button color="falcon-default" size="sm" className="ml-2">
-                Apply
-              </Button>
-            </InputGroup>
-          ) : (
-            <Fragment>
-              <ButtonIcon icon="plus" transform="shrink-3 down-2" color="falcon-default" size="sm">
-                New
-              </ButtonIcon>
-              <ButtonIcon icon="filter" transform="shrink-3 down-2" color="falcon-default" size="sm" className="mx-2">
-                Filter
-              </ButtonIcon>
-              <ButtonIcon icon="external-link-alt" transform="shrink-3 down-2" color="falcon-default" size="sm">
-                Export
-              </ButtonIcon>
-            </Fragment>
-          )}
-        </FalconCardHeader>
-        <CardBody className="p-0">
-          <PurchasesTable setIsSelected={setIsSelected} />
-        </CardBody>
-      </Card>
-      <Row noGutters>
-        <Col lg="4" className="pr-lg-2">
-          <ActiveUsersBarChart />
-        </Col>
-        <Col lg="8" className="pl-lg-2">
-          <ActiveUsersMap />
-        </Col>
-      </Row>
+      <EnergysLineChart />
+      <ChildSpacesTable />
+      
     </Fragment>
   );
 };
