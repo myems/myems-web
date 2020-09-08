dd1d2820ef96211f8384bc33f648d775904c42dc
guangyuzhang <13621160019@163.com>
2020-09-08 15:47:04 +0800

    added GET Tenants by SpaceID to Tenant* components

--- src/components/MyEMS/Tenant/TenantLoad.js
+++ src/components/MyEMS/Tenant/TenantLoad.js
@@ -55,8 +55,9 @@ const TenantLoad = ({ setRedirect, setRedirectUrl, t }) => {
   // State
   const [selectedSpaceName, setSelectedSpaceName] = useState(undefined);
   const [selectedSpaceID, setSelectedSpaceID] = useState(undefined);
+  const [tenantList, setTenantList] = useState([]);
+  const [selectedTenant, setSelectedTenant] = useState(undefined);
   const [comparisonType, setComparisonType] = useState('month-on-month');
-  const [tenant, setTenant] = useState(undefined);
   const [basePeriodBeginsDatetime, setBasePeriodBeginsDatetime] = useState(current_moment.clone().subtract(1, 'months').startOf('month'));
   const [basePeriodEndsDatetime, setBasePeriodEndsDatetime] = useState(current_moment.clone().subtract(1, 'months'));
   const [basePeriodBeginsDatetimeDisabled, setBasePeriodBeginsDatetimeDisabled] = useState(true);
@@ -92,6 +93,41 @@ const TenantLoad = ({ setRedirect, setRedirectUrl, t }) => {
         setCascaderOptions(json);
         setSelectedSpaceName([json[0]].map(o => o.label));
         setSelectedSpaceID([json[0]].map(o => o.value));
+        // get Tenants by root Space ID
+        let isResponseOK = false;
+        fetch(baseURL + '/spaces/' + [json[0]].map(o => o.value) + '/tenants', {
+          method: 'GET',
+          headers: {
+            "Content-type": "application/json",
+            "User-UUID": getCookieValue('user_uuid'),
+            "Token": getCookieValue('token')
+          },
+          body: null,
+
+        }).then(response => {
+          if (response.ok) {
+            isResponseOK = true;
+          }
+          return response.json();
+        }).then(json => {
+          if (isResponseOK) {
+            json = JSON.parse(JSON.stringify([json]).split('"id":').join('"value":').split('"name":').join('"label":'));
+            console.log(json)
+            setTenantList(json[0]);
+            if (json[0].length > 0) {
+              setSelectedTenant(json[0][0].value);
+              setIsDisabled(false);
+            } else {
+              setSelectedTenant(undefined);
+              setIsDisabled(true);
+            }
+          } else {
+            toast.error(json.description)
+          }
+        }).catch(err => {
+          console.log(err);
+        });
+        // end of get Tenants by root Space ID
       } else {
         toast.error(json.description);
       }
@@ -100,12 +136,7 @@ const TenantLoad = ({ setRedirect, setRedirectUrl, t }) => {
     });
 
   }, []);
-  const tenantList = [
-    { value: 1, label: 'Gucci 古驰' },
-    { value: 2, label: 'Longines浪琴' },
-    { value: 3, label: 'Starbucks星巴克' },
-    { value: 4, label: 'Versace/范思哲' }];
-
+  
   const labelClasses = 'ls text-uppercase text-600 font-weight-semi-bold mb-0';
 
   const tenantLineChartLabels = [
@@ -280,9 +311,42 @@ const TenantLoad = ({ setRedirect, setRedirectUrl, t }) => {
   }];
 
   let onSpaceCascaderChange = (value, selectedOptions) => {
-    console.log(value, selectedOptions);
     setSelectedSpaceName(selectedOptions.map(o => o.label).join('/'));
     setSelectedSpaceID(value[value.length - 1]);
+
+    let isResponseOK = false;
+    fetch(baseURL + '/spaces/' + value[value.length - 1] + '/tenants', {
+      method: 'GET',
+      headers: {
+        "Content-type": "application/json",
+        "User-UUID": getCookieValue('user_uuid'),
+        "Token": getCookieValue('token')
+      },
+      body: null,
+
+    }).then(response => {
+      if (response.ok) {
+        isResponseOK = true;
+      }
+      return response.json();
+    }).then(json => {
+      if (isResponseOK) {
+        json = JSON.parse(JSON.stringify([json]).split('"id":').join('"value":').split('"name":').join('"label":'));
+        console.log(json)
+        setTenantList(json[0]);
+        if (json[0].length > 0) {
+          setSelectedTenant(json[0][0].value);
+          setIsDisabled(false);
+        } else {
+          setSelectedTenant(undefined);
+          setIsDisabled(true);
+        }
+      } else {
+        toast.error(json.description)
+      }
+    }).catch(err => {
+      console.log(err);
+    });
   }
 
 
@@ -357,6 +421,7 @@ const TenantLoad = ({ setRedirect, setRedirectUrl, t }) => {
     e.preventDefault();
     console.log('handleSubmit');
     console.log(selectedSpaceID);
+    console.log(selectedTenant);
   };
 
 
@@ -387,10 +452,10 @@ const TenantLoad = ({ setRedirect, setRedirectUrl, t }) => {
               </Col>
               <Col xs="auto">
                 <FormGroup>
-                  <Label className={labelClasses} for="tenant">
+                  <Label className={labelClasses} for="tenantSelect">
                     {t('Tenant')}
                   </Label>
-                  <CustomInput type="select" id="tenant" name="tenant" value={tenant} onChange={({ target }) => setTenant(target.value)}
+                  <CustomInput type="select" id="tenantSelect" name="tenantSelect" onChange={({ target }) => setSelectedTenant(target.value)}
                   >
                     {tenantList.map((tenant, index) => (
                       <option value={tenant.value} key={tenant.value}>
@@ -486,7 +551,7 @@ const TenantLoad = ({ setRedirect, setRedirectUrl, t }) => {
                 <FormGroup>
                   <br></br>
                   <ButtonGroup id="submit">
-                    <Button color="success" >{t('Submit')}</Button>
+                  <Button color="success" disabled={isDisabled} >{t('Submit')}</Button>
                   </ButtonGroup>
                 </FormGroup>
               </Col>
