a8e8cc63c4e901870db619fb0f5f37879c81224b
guangyuzhang <13621160019@163.com>
2021-01-12 18:23:49 +0800

    added state to disable/enable  submit button

--- src/components/MyEMS/Shopfloor/ShopfloorEnergyCategory.js
+++ src/components/MyEMS/Shopfloor/ShopfloorEnergyCategory.js
@@ -68,7 +68,10 @@ const ShopfloorEnergyCategory = ({ setRedirect, setRedirectUrl, t }) => {
   const [reportingPeriodBeginsDatetime, setReportingPeriodBeginsDatetime] = useState(current_moment.clone().startOf('month'));
   const [reportingPeriodEndsDatetime, setReportingPeriodEndsDatetime] = useState(current_moment);
   const [cascaderOptions, setCascaderOptions] = useState(undefined);
-  const [isDisabled, setIsDisabled] = useState(true);
+        
+  // Submit button status
+  const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false);
+
   //Results
   const [timeOfUseShareData, setTimeOfUseShareData] = useState([]);
   const [TCEShareData, setTCEShareData] = useState([]);
@@ -136,10 +139,12 @@ const ShopfloorEnergyCategory = ({ setRedirect, setRedirectUrl, t }) => {
             setShopfloorList(json[0]);
             if (json[0].length > 0) {
               setSelectedShopfloor(json[0][0].value);
-              setIsDisabled(false);
+              // enable submit button
+              setSubmitButtonDisabled(false);
             } else {
               setSelectedShopfloor(undefined);
-              setIsDisabled(true);
+              // disable submit button
+              setSubmitButtonDisabled(true);
             }
           } else {
             toast.error(json.description)
@@ -186,10 +191,12 @@ const ShopfloorEnergyCategory = ({ setRedirect, setRedirectUrl, t }) => {
         setShopfloorList(json[0]);
         if (json[0].length > 0) {
           setSelectedShopfloor(json[0][0].value);
-          setIsDisabled(false);
+          // enable submit button
+          setSubmitButtonDisabled(false);
         } else {
           setSelectedShopfloor(undefined);
-          setIsDisabled(true);
+          // disable submit button
+          setSubmitButtonDisabled(true);
         }
       } else {
         toast.error(json.description)
@@ -278,6 +285,9 @@ const ShopfloorEnergyCategory = ({ setRedirect, setRedirectUrl, t }) => {
     console.log(reportingPeriodBeginsDatetime.format('YYYY-MM-DDTHH:mm:ss'));
     console.log(reportingPeriodEndsDatetime.format('YYYY-MM-DDTHH:mm:ss'));
 
+    // disable submit button
+    setSubmitButtonDisabled(true);
+        
     // Reinitialize tables
     setDetailedDataTableData([]);
     
@@ -301,6 +311,10 @@ const ShopfloorEnergyCategory = ({ setRedirect, setRedirectUrl, t }) => {
       if (response.ok) {
         isResponseOK = true;
       }
+
+      // enable submit button
+      setSubmitButtonDisabled(false);
+
       return response.json();
     }).then(json => {
       if (isResponseOK) {
@@ -601,7 +615,7 @@ const ShopfloorEnergyCategory = ({ setRedirect, setRedirectUrl, t }) => {
                 <FormGroup>
                   <br></br>
                   <ButtonGroup id="submit">
-                    <Button color="success" disabled={isDisabled} >{t('Submit')}</Button>
+                    <Button color="success" disabled={submitButtonDisabled} >{t('Submit')}</Button>
                   </ButtonGroup>
                 </FormGroup>
               </Col>
