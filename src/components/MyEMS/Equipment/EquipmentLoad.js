1784909c3cdda264c351f9a2ca70cc2037b0462f
guangyuzhang <13621160019@163.com>
2021-01-13 10:52:12 +0800

    added state to disable/enable  submit button

--- src/components/MyEMS/Equipment/EquipmentLoad.js
+++ src/components/MyEMS/Equipment/EquipmentLoad.js
@@ -67,7 +67,10 @@ const EquipmentLoad = ({ setRedirect, setRedirectUrl, t }) => {
   const [reportingPeriodBeginsDatetime, setReportingPeriodBeginsDatetime] = useState(current_moment.clone().startOf('month'));
   const [reportingPeriodEndsDatetime, setReportingPeriodEndsDatetime] = useState(current_moment);
   const [cascaderOptions, setCascaderOptions] = useState(undefined);
-  const [isDisabled, setIsDisabled] = useState(true);
+  
+  // Submit button status
+  const [submitButtonDisabled, setSubmitButtonDisabled] = useState(true);
+  
   //Results
   const [cardSummaryList, setCardSummaryList] = useState([]);
   const [equipmentLineChartLabels, setEquipmentLineChartLabels] = useState([]);
@@ -129,10 +132,12 @@ const EquipmentLoad = ({ setRedirect, setRedirectUrl, t }) => {
             setEquipmentList(json[0]);
             if (json[0].length > 0) {
               setSelectedEquipment(json[0][0].value);
-              setIsDisabled(false);
+              // enable submit button
+              setSubmitButtonDisabled(false);
             } else {
               setSelectedEquipment(undefined);
-              setIsDisabled(true);
+              // disable submit button
+              setSubmitButtonDisabled(true);
             }
           } else {
             toast.error(json.description)
@@ -178,10 +183,12 @@ const EquipmentLoad = ({ setRedirect, setRedirectUrl, t }) => {
         setEquipmentList(json[0]);
         if (json[0].length > 0) {
           setSelectedEquipment(json[0][0].value);
-          setIsDisabled(false);
+          // enable submit button
+          setSubmitButtonDisabled(false);
         } else {
           setSelectedEquipment(undefined);
-          setIsDisabled(true);
+          // disable submit button
+          setSubmitButtonDisabled(true);
         }
       } else {
         toast.error(json.description)
@@ -271,6 +278,9 @@ const EquipmentLoad = ({ setRedirect, setRedirectUrl, t }) => {
     console.log(reportingPeriodBeginsDatetime.format('YYYY-MM-DDTHH:mm:ss'));
     console.log(reportingPeriodEndsDatetime.format('YYYY-MM-DDTHH:mm:ss'));
 
+    // disable submit button
+    setSubmitButtonDisabled(true);
+
     // Reinitialize tables
     setDetailedDataTableData([]);
     
@@ -294,6 +304,10 @@ const EquipmentLoad = ({ setRedirect, setRedirectUrl, t }) => {
       if (response.ok) {
         isResponseOK = true;
       }
+
+      // enable submit button
+      setSubmitButtonDisabled(false);
+
       return response.json();
     }).then(json => {
       if (isResponseOK) {
@@ -536,7 +550,7 @@ const EquipmentLoad = ({ setRedirect, setRedirectUrl, t }) => {
                 <FormGroup>
                   <br></br>
                   <ButtonGroup id="submit">
-                    <Button color="success" disabled={isDisabled} >{t('Submit')}</Button>
+                    <Button color="success" disabled={submitButtonDisabled} >{t('Submit')}</Button>
                   </ButtonGroup>
                 </FormGroup>
               </Col>
